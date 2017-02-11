ae378358dd5994f03d7590c499945fd127e1db7d
goujonpa <goujonpa@gmail.com>
2017-02-11 17:20:56 +0100

    update init db

--- cassandra/init_db.py
+++ cassandra/init_db.py
@@ -1,6 +1,12 @@
 from cassandra.cluster import Cluster
 
-from cassandra_settings import cass_settings
+cass_settings = {
+    'CASS_CONNECT_POINTS': ['127.0.0.1'],
+    'CASS_PORT': 9042,
+    'CASS_KEYSPACE': 'ais_datas',
+    'CASS_STRATEGY': 'SimpleStrategy',
+    'CASS_REPL': 1
+}
 
 # Init DB
 cluster = Cluster(
